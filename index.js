34f8207f0fca119f36b8fb20608f99a78bbbd989
Spencer Brooks <spencer@databit.io>
2019-03-05 16:50:58 -0700

    show current users

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 var app = require('express')()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+let users = []
 
 io.on('connection', function (socket) {
   console.log('a user has connected')
   socket.on('disconnect', function () {
     console.log('user disconnected')
+    users = users.filter((userObj) => {
+      return userObj['id'] !== socket.id
+    })
+    console.log(users)
+    io.emit('leave room', users)
   })
-  socket.on('chat message', function (msg) {
-    console.log('message: ' + msg)
-    io.emit('chat message', msg)
+  socket.on('chat message', function (msg, username) {
+    // console.log('message: ' + msg + ' from: ' + username)
+    io.emit('chat message', msg, username)
+  })
+  socket.on('join room', function (username) {
+    let obj = new Object()
+    obj.name = username
+    obj.id = socket.id
+    users.push(obj)
+    io.emit('join room', users)
+  })
+  socket.on('error', (error) => {
+    console.log('Error(s): ' + error)
   })
 })
 
