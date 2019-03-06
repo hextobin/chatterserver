c8f0fb02bdff3ebe384513262e123e623891ff2f
Spencer Brooks <spencer@databit.io>
2019-03-06 11:52:13 -0700

    add origin to socket io

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var app = require('express')()
 var http = require('http').Server(app)
-var io = require('socket.io')(http)
+var io = require('socket.io')(http, {origins: 'localhost:3001'})
 let users = []
 
 io.on('connection', function (socket) {
